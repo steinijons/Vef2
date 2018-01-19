3ca0c5d531c08e760e1d83da1f29d28d55a9ebd9
fanney83 <fanneyv12@ru.is>
2018-01-19 15:11:26 +0000

    lagaði 8 smá

--- assignment1/makeBelieve.js
+++ assignment1/makeBelieve.js
@@ -67,14 +67,11 @@
   this.ancestor = function (query) {
     var ancestorList = [];
     var queryAncestor = document.querySelectorAll(query);
-    //console.log(querySelectorAll(grandParentList));
 
     for (var e = 0; e < this.elements.length; e++) {
       for (var i = 0; i < queryAncestor.length; i++) {
-        console.log(this.grandParent(query));
         var grandFunction = this.grandParent();
         if(queryAncestor[i] == this.elements[i].grandFunction){
-          console.log(this.elements[i].grandParent());
           ancestorList.push(this.elements[i])
         }
       }
@@ -99,21 +96,29 @@
 
 // 8 - Append
   this.append = function (html) {
-    var newElem = document.createElement('template');
-    for (var i = 0; i < this.length; i++) {
-      var node = this.elements[i];
-      if(typeof html === 'string') {
-        newElem.innerText = html;
-        node.appendChild(newElem.content);
-      }
-      if(html instanceof Node) {
-        node.appendChild(html)
+    // An empty element, new HTMl will be appended to this
+    // fannst þetta meika smá sens
+    // https://www.w3schools.com/js/js_htmldom_nodes.asp
+    // https://stackoverflow.com/questions/494143/creating-a-new-dom-element-from-an-html-string-using-built-in-dom-methods-or-pro
+    var node = document.createElement('template');
+    if (typeof html === 'string') {
+      for (var i = 0; i < this.length; i++) {
+        var currElem = this.elements[i];
+        node.textContent = html;
+        currElem.appendChild(node);
       }
     }
-    console.log(node);
-
+    if(html instanceof Node) {
+      for (var i = 0; i < this.length; i++) {
+        var currElem = this.elements[i];
+        currElem.appendChild(html);
+      }
+    }
   }
-  // 9 -
+  // 9 - Prepend
+  this.prepend = function () {
+    
+  }
 
 // End of fun
   };
@@ -132,7 +137,7 @@
 })();
 
 
-/*
+
 var appender = __('.the-appender').append('<p>I am the appended paragraph!</p>');
 var appendler = __('.the-appender').append(
   document.createElement('p')
@@ -140,7 +145,7 @@ var appendler = __('.the-appender').append(
       document.createTextNode('I am an appended paragraph!')
     )
 );
-//console.log(appender);
+console.log(appender);
 console.log(appendler);
 
 var handler = __('#password').onClick(function (evt) {
@@ -154,4 +159,3 @@ var insertText = __('#shakespeare-novel').insertText('To be, or not to be: this 
 //console.log('Ancestors');
 //var ancestor = __('#usrnm').ancestor('.ancestr');
 //console.log(ancestor);
-/*
