5ab9d42ed7b19c297fdc3c3b9709c20491674c97
Anitasol <anitaj15@ru.is>
2018-02-28 13:54:34 +0000

    eftir aukaf.

---
+++ assignment4/PizzerioUno/src/components/Menu/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Pizza from '../Pizza/Pizza';
+import { getAllPizzas } from '../../actions/pizzaActions';
+
+class Menu extends React.Component {
+    ComponentDidMount() {
+        const { getAllPizzas } = this.props;
+        getAllPizzas();
+    }
+    render() {
+        const { pizza } = this.props;
+        return (
+            <div className="container">
+                {pizza.map(p => <Pizza key={p.id} pizza={p} />)}
+            </div>
+        );
+    }
+};
+
+const mapStateToProps = ({ pizza }) => {
+    return { pizza }
+}
+
+export default connect(mapStateToProps, { getAllPizzas })(Menu);
